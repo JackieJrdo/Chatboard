1753385420c623f193deee85e8872ed9052cb8a9
Jackie Jurado <jackiejrdo@gmail.com>
2023-11-15 01:19:03 -0500

    config

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB:
+    'mongodb+srv://jackiejrdo:Jack412331@cluster0v2.7gmsjfe.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+};
